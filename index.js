a608a737090482e6e91ca879dcddc9059089c87a
Rachel Diaz <Rachel7113@gmail.com>
2021-03-18 17:07:39 -0400

    Added viewEmployees function - displays table in terminal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
-const cTable = require("console.table");
+require("console.table");
+const connection = require("./config/connection");
 
 const menuOptions = () => {
     inquirer
@@ -8,7 +9,32 @@ const menuOptions = () => {
                 type: 'list',
                 name: "options",
                 message: "What would you like to do?",
-                choices: ["Add Employees", "Add Roles", "Add Departments", "View Employees", "View Roles", "View Departments", "Update Employee Role", "Exit"]
+                choices: ['Add Employees', 'Add Roles', 'Add Departments', 'View Employees', 'View Roles', 'View Departments', 'Update Employee Role', 'Exit']
             }
-        ])
+        ]).then(answer => {
+            console.log(answer)
+            switch (answer.options) {
+                case 'View Employees':
+                    viewEmployees();
+                    break;
+
+                case 'Exit':
+                    break;
+            }
+        })
+}
+
+menuOptions();
+
+function viewEmployees() {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id
+    FROM employee
+    ORDER BY employee.id`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log(res);
+        console.table(res)
+        connection.end();
+        menuOptions();
+    })
 }
