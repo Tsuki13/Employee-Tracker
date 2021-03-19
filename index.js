7d40db6e21c10ff1c2230f60603520ae25fa1f23
Rachel Diaz <Rachel7113@gmail.com>
2021-03-19 13:40:42 -0400

    Added view departments, view roles, and exit functions

--- index.js
+++ index.js
@@ -18,23 +18,61 @@ const menuOptions = () => {
                     viewEmployees();
                     break;
 
+                case 'View Roles':
+                    viewRoles();
+                    break;
+
+                case 'View Departments':
+                    viewDepartments();
+                    break;
+
                 case 'Exit':
+                    exitSession();
                     break;
             }
         })
 }
 
-menuOptions();
-
 function viewEmployees() {
-    const query = `SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id
-    FROM employee
-    ORDER BY employee.id`;
+    const query = `SELECT employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.salary, role.title, department_id
+    FROM employee 
+    INNER JOIN role 
+    ON employee.role_id = role.id`;
     connection.query(query, (err, res) => {
         if (err) throw err;
-        console.log(res);
-        console.table(res)
-        connection.end();
+        console.table(res);
         menuOptions();
     })
 }
+
+function viewRoles() {
+    const query = `SELECT role.title, role.salary, role.department_id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id
+    FROM role
+    INNER JOIN employee
+    ON role.id = employee.role_id
+    ORDER BY role.id`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menuOptions();
+    })
+}
+
+function viewDepartments() {
+    const query = `SELECT department.id, department.name, employee.id, employee.first_name, employee.last_name
+    FROM department
+    INNER JOIN employee
+    ON employee.department_id = department.id`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menuOptions();
+    })
+}
+
+function exitSession() {
+    console.log(`Session ended by id #${connection.threadId}`)
+    connection.end();
+}
+
+menuOptions();
